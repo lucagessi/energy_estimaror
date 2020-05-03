186415b57b05a948d6f4f9be164bf4cd901296e4
Luca Gessi <luca.gessi90@gmail.com>
2020-05-03 15:21:49 +0200

    Tested new ConfigurableLoad

--- src/typescript/test.ts
+++ src/typescript/test.ts
@@ -3,33 +3,44 @@ console.log('Inside test.ts');
 import {ConfigurableLoad, BasicLoadContainer,PhysicalLoad, ILoad} from './load';
 
 var uC  : PhysicalLoad = new PhysicalLoad();
+var rf_module  : PhysicalLoad = new PhysicalLoad();
 var led : PhysicalLoad = new PhysicalLoad();
 
-var stm32 : ConfigurableLoad = new ConfigurableLoad();
-var redLed: ConfigurableLoad = new ConfigurableLoad();
+var stm32 :  ConfigurableLoad = new ConfigurableLoad();
+var redLed:  ConfigurableLoad = new ConfigurableLoad();
+var blueLed: ConfigurableLoad = new ConfigurableLoad();
+var Ble:     ConfigurableLoad = new ConfigurableLoad();
 
 var group : BasicLoadContainer = new BasicLoadContainer();
 var pnt : ILoad;
 
-stm32.load = uC;
-redLed.load = led;
-
 uC.addConfiguration({ consumption:1.2, key:"On"} )
 uC.addConfiguration({ consumption:0.001, key:"Off"} )
-
 led.addConfiguration({ consumption:10, key:"On"} )
 led.addConfiguration({ consumption:0.001, key:"Off"} )
+rf_module.addConfiguration({ consumption:5.6, key:"TX/RX"} )
+rf_module.addConfiguration({ consumption:0.010, key:"Sleep"} )
+
+stm32.load = uC;
+redLed.load = led;
+blueLed.load = led;
+Ble.load = rf_module;
 
 group.add(stm32)
 group.add(redLed)
-group.remove(stm32)
+group.add(blueLed)
+group.add(Ble)
 
 console.log(uC.serialize)
 
-console.log("uC consumption: "+stm32.consumption)
-console.log("Setting uC config: On..")
+console.log("Group consumption: "+group.consumption)
+console.log("Setting configurations...")
+
+Ble.setConfiguration("TX/RX")
 stm32.setConfiguration ("On")
 redLed.setConfiguration("On")
+blueLed.setConfiguration('On')
+
 console.log("uC consumption: "+stm32.consumption)
 console.log("led consumption: "+redLed.consumption)
 
